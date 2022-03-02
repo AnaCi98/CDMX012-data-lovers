d7032f7816b1cc957b857196deec8f35dd6dcb07
AnaCif98 <inv0320@innovaccion.mx>
2022-03-02 09:34:33 -0700

    agregué función de ordenado al main

--- src/data.js
+++ src/data.js
@@ -1,15 +1,21 @@
 //La funcion ordenar toma la data y la ordena alfabeticamente, nos regresa la data ordenada alfabeticamente
-export const ordenar = (data) => {
-  let nombresPersonaje = data.sort((a,b)=>{
-      if(a.name>b.name){ // "b" va primero que "a"
-        return 1;
-      }
-      if(a.name<b.name){ //es como decir que "a" va primero que "b"
-      return -1;
-      }
-  });
+export const ordenarABC = (data) => {
+let nombresPersonaje = data.sort((a,b)=>{
+  if(a.name<b.name){ //es como decir que "a" va primero que "b"
+    return -1;
+  }
+  if(a.name>b.name){ // "b" va primero que "a"
+      return 1;
+    }
+}
+  );
   return nombresPersonaje;
 };
+
+export let porNombre = (data,condition) => {
+return data.filter(personaje => {return personaje.name.toLowerCase() === condition});
+}
+
 
 export const anotherExample = () => {
   return 'OMG';
